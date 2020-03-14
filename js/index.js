4fe5b22c567f0ff64b09ede71f10dc713e7e85b0
canwebe <61940618+canwebe@users.noreply.github.com>
2020-03-14 19:42:26 +0530

    Adding initial file

---
+++ js/index.js
@@ -0,0 +1,86 @@
+const main=document.querySelector('.main');
+const selector=document.querySelector('#srcselect');
+const btn=document.querySelector('.searchButton');
+const headtag=document.querySelector('.hdrtag');
+const body=document.querySelector('body');
+const sortselector=document.querySelector('.sort');
+const load=document.querySelector('.load');
+const api="471a578390434f8cada92ab15e4e8812";
+const defsrc="google-news-in";
+const defsort="publishedAt";
+const defpage=1;
+var inn=0;
+
+window.addEventListener('load',async ()=>{
+  updateNews();
+  await updateSrc();
+  selector.value=defsrc;
+  selector.addEventListener('change',a=>{
+    updateNews(a.target.value);
+  })
+  sortselector.addEventListener('change',b=>{
+    updateSearch(b.target.value);
+    console.log(b.target.value);
+  })
+ 
+document.querySelector('input').onkeyup=e=>{
+  if(e.keyCode===13)
+      updateSearch(); 
+}
+btn.onclick=updateSearch;
+  
+  async function updateSearch(srtby=defsort,page=defpage){
+     const srsTerm=document.querySelector('.searchTerm').value;
+      selector.style.display="none";  
+     headtag.innerHTML=`Search Results for <strong>${srsTerm}</strong>`;
+    if(sortselector.style.display==='none'){
+      sortselector.style.display="block";
+    }
+     const res= await fetch(`http://newsapi.org/v2/everything?q=${srsTerm}&sortBy=${srtby}&pageSize=15&page=${page}&language=en&apiKey=${api}`);
+     const json=await res.json();
+    main.innerHTML=json.articles.map(createArticles).join('\n');
+  while(json.totalResults>15){
+      inn++;
+      if(load.style.display==="none")
+        {
+          load.style.display="block";
+        }
+      else{
+           load.style.display="none";
+        }
+//  document.querySelector('.searchfor').addEventListener('click',function(){
+//    alert(inn);
+//    updateSearch(page=inn);
+//  });
+ 
+    }
+    
+}
+
+});
+
+async function updateSrc(){
+  const res= await fetch(`http://newsapi.org/v2/sources?apiKey=${api}`);
+  const json=await res.json();
+  selector.innerHTML=json.sources.map(src=>
+    `<option value="${src.id}">${src.name}</option>`
+  ).join('\n');  
+}
+async function updateNews(source=defsrc){
+  const res= await fetch(`http://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${api}`);
+  const json=await res.json();
+  main.innerHTML=json.articles.map(createArticles).join('\n');
+}
+
+function createArticles(article){
+      return `
+   <div class="article">
+    <a href="${article.url}">
+      <h2>${article.title}</h2>
+      <img src="${article.urlToImage}">
+      <p>${article.description}</p>
+    </a>
+   </div>
+`;
+  
+}
